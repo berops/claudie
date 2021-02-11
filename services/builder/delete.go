64e22723ef2ea0dbf43664de4f82c1338224ced9
samuelstolicny <stolicnysamuel@gmail.com>
2021-02-11 08:54:47 +0100

    Fix func diffenrence

--- services/builder/delete.go
+++ services/builder/delete.go
@@ -56,28 +56,21 @@ func deleteNodes(project *pb.Project) error {
 	return nil
 }
 
+// difference returns slice of strings which are in the first slice but not in the second slice
 func difference(slice1 []string, slice2 []string) []string {
 	var diff []string
 
-	// Loop two times, first to find slice1 strings not in slice2,
-	// second loop to find slice2 strings not in slice1
-	for i := 0; i < 2; i++ {
-		for _, s1 := range slice1 {
-			found := false
-			for _, s2 := range slice2 {
-				if s1 == s2 {
-					found = true
-					break
-				}
-			}
-			// String not found. We add it to return slice
-			if !found {
-				diff = append(diff, s1)
+	for _, s1 := range slice1 {
+		found := false
+		for _, s2 := range slice2 {
+			if s1 == s2 {
+				found = true
+				break
 			}
 		}
-		// Swap the slices, only if it was the first loop
-		if i == 0 {
-			slice1, slice2 = slice2, slice1
+		// String not found. We add it to return slice
+		if !found {
+			diff = append(diff, s1)
 		}
 	}
 
