7c858d126a50f2c4484155b13b014d188ad8d422
samuelstolicny <stolicnysamuel@gmail.com>
2021-04-26 13:34:06 +0200

    Save new desiredState with SaveConfig

--- services/scheduler/scheduler.go
+++ services/scheduler/scheduler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -11,8 +10,6 @@ import (
 	"github.com/Berops/platform/proto/pb"
 	cbox "github.com/Berops/platform/services/context-box/client"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"gopkg.in/yaml.v3"
 )
 
@@ -49,6 +46,8 @@ type Master struct {
 	Image      string `yaml:"image"`
 	DiskSize   uint32 `yaml:"disk_size"`
 	Zone       string `yaml:"zone"`
+	Location   string `yaml:"location"`
+	Datacenter string `yaml:"datacenter"`
 }
 
 type Worker struct {
@@ -57,37 +56,75 @@ type Worker struct {
 	Image      string `yaml:"image"`
 	DiskSize   uint32 `yaml:"disk_size"`
 	Zone       string `yaml:"zone"`
+	Location   string `yaml:"location"`
+	Datacenter string `yaml:"datacenter"`
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-func createDesiredState(res *pb.GetConfigResponse) (*pb.SaveConfigRequest, error) {
-	for _, config := range res.GetConfigs() { //res is slice of config
-		//Create yaml manifest
-		d := []byte(config.GetManifest())
-		err := ioutil.WriteFile("manifest.yaml", d, 0644)
-		if err != nil {
-			return nil, status.Errorf(
-				codes.Internal,
-				fmt.Sprintf("Cannot create yaml manifest file: %v", err),
-			)
-		}
-		//Parse yaml to protobuf and create desiredState
-		var desiredState Manifest
-		yamlFile, err := ioutil.ReadFile("manifest.yaml")
-		err = yaml.Unmarshal(yamlFile, &desiredState)
-		if err != nil {
-			return nil, err
-		}
-		log.Println(desiredState)
-
-		//Remove yaml manifest after loading
-		err = os.Remove("manifest.yaml")
-		if err != nil {
-			return nil, err
-		}
+func createDesiredState(config *pb.Config) *pb.Config {
+	//Create yaml manifest
+	d := []byte(config.GetManifest())
+	err := ioutil.WriteFile("manifest.yaml", d, 0644)
+	if err != nil {
+		log.Fatalln("Error while creating manifest.yaml file", err)
 	}
-	return nil, nil
+	//Parse yaml to protobuf and create desiredState
+	var desiredState Manifest
+	yamlFile, err := ioutil.ReadFile("manifest.yaml")
+	if err != nil {
+		log.Fatalln("error while reading maninfest.yaml file", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &desiredState)
+	if err != nil {
+		log.Fatalln("error while unmarshalling yaml file", err)
+	}
+	//Remove yaml manifest after loading
+	err = os.Remove("manifest.yaml")
+	if err != nil {
+		log.Fatalln("error while removing maninfest.yaml file", err)
+	}
+
+	clusters := []*pb.Cluster{}
+	for _, cluster := range desiredState.Clusters {
+		nodePools := []*pb.NodePool{}
+		for _, nodePool := range cluster.NodePools {
+			nodePools = append(nodePools, &pb.NodePool{
+				Name:   nodePool.Name,
+				Region: nodePool.Region,
+				Master: &pb.Node{
+					Count:      uint32(nodePool.Master.Count),
+					ServerType: nodePool.Master.ServerType,
+					Image:      nodePool.Master.Image,
+					DiskSize:   nodePool.Master.DiskSize,
+					Zone:       nodePool.Master.Zone,
+					Location:   nodePool.Master.Location,
+					Datacenter: nodePool.Master.Datacenter,
+				},
+			})
+		}
+
+		clusters = append(clusters, &pb.Cluster{
+			Name:       cluster.Name,
+			Kubernetes: cluster.Kubernetes,
+			Network:    cluster.Network,
+			NodePools:  nodePools,
+		})
+	}
+
+	return &pb.Config{
+		Id:       config.GetId(),
+		Name:     config.GetName(),
+		Manifest: config.GetManifest(),
+		DesiredState: &pb.Project{
+			Name: desiredState.Name,
+			Credentials: map[string]string{
+				"gcp":     desiredState.PublicCloudCredentials.Gcp,
+				"hetzner": desiredState.PublicCloudCredentials.Hetzner,
+			},
+			Clusters: clusters,
+		},
+	}
 }
 
 func main() {
@@ -101,12 +138,20 @@ func main() {
 	// Creating the client
 	c := pb.NewContextBoxServiceClient(cc)
 
+	//Infinite FOR cycle gets all configs from the context box every defined number of seconds
 	for { // TODO: Maybe goroutines here?
-		res, err := cbox.GetConfig(c) //Get all configs from the database. It is a grpc call.
+		res, err := cbox.GetConfig(c) //Get all configs from the database. It is a grpc call to the context-box
 		if err != nil {
 			log.Fatalln("Error while getting config from the Scheduler", err)
 		}
-		createDesiredState(res)
+		for _, config := range res.GetConfigs() {
+			config = createDesiredState(config)
+			//fmt.Println(config.GetDesiredState())
+			err := cbox.SaveConfig(c, config)
+			if err != nil {
+				log.Fatalln("Error while saving config", err)
+			}
+		}
 		time.Sleep(5 * time.Second)
 	}
 }
