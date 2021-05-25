ed677ddf4be3bf81a33be4790aeceddd15546884
samuelstolicny <stolicnysamuel@gmail.com>
2021-05-25 13:19:57 +0200

    Rename server.go to builder.go

--- services/builder/builder.go
+++ services/builder/builder.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net"
@@ -10,43 +9,8 @@ import (
 
 	"github.com/Berops/platform/ports"
 	"github.com/Berops/platform/proto/pb"
-	"github.com/Berops/platform/serializer"
 	"google.golang.org/grpc"
 )
-
-type server struct{}
-
-func (*server) Build(_ context.Context, req *pb.Project) (*pb.Project, error) {
-	//Check if project has a kubeconfig
-	if req.GetCluster().GetKubeconfig() != nil { // Kubeconfig exists
-		fmt.Println("Cluster already has a kubeconfig file")
-		//Check if there are any different nodes and delete them
-		err := deleteNodes(req)
-		if err != nil {
-			log.Fatalln("Error while deleting nodes from cluster", err)
-		}
-	}
-
-	//Call TWK modules
-	project, err := flow(req)
-	if err != nil {
-		log.Fatalln("Error in flow: ", err)
-	}
-
-	// Saving file TEMPORARY
-	err = serializer.WriteProtobufToBinaryFile(project, "../../tmp/project.bin")
-	if err != nil {
-		log.Fatalln("Error while saving the project a binary file", err)
-	}
-	log.Println("Project has been saved to a binary file")
-	err = serializer.WriteProtobufToJSONFile(project, "../../tmp/project.json")
-	if err != nil {
-		log.Fatalln("Error while saving the project a json file", err)
-	}
-	log.Println("Project has been saved to a json file")
-
-	return project, nil //return response(project) to the client(Reconcilliator)
-}
 
 // flow permorms the sequence of gRPC calls to Terraformer, Wireguardian, KubeEleven modules (TWK)
 func flow(project *pb.Project) (*pb.Project, error) {
