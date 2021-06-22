2ab703caff83c1a3368f2f6d59c0ad54e5e75e1f
samuelstolicny <stolicnysamuel@gmail.com>
2021-06-22 15:04:50 +0200

    Delete old and implement a new ssh-key gen function

--- services/scheduler/scheduler_test.go
+++ services/scheduler/scheduler_test.go
@@ -1,30 +1,11 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_generateRSAKeyPair(t *testing.T) {
-	var tests []struct {
-		name  string
-		want  string
-		want1 string
-	}
-	tests = append(tests, struct {
-		name  string
-		want  string
-		want1 string
-	}{name: "Pokus", want: "Something", want1: "Something else"})
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := generateRSAKeyPair()
-			t.Log("Private key", got)
-			t.Log("Public key", got1)
-			if got != tt.want {
-				t.Errorf("generateRSAKeyPair() got = %v, want %v", got, tt.want)
-			}
-			if got1 != tt.want1 {
-				t.Errorf("generateRSAKeyPair() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
+func TestMakeSSHKeyPair(t *testing.T) {
+	priv, pub := MakeSSHKeyPair()
+	t.Log(priv)
+	t.Log(pub)
 }
