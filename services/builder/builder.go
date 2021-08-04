3823c491766bed81f0ac9870bca2233f2ff68739
Alireza Salary <alireza.salary@outlook.com>
2021-08-04 14:41:21 +0430

    improve builder errors

--- services/builder/builder.go
+++ services/builder/builder.go
@@ -19,65 +19,82 @@ import (
 	"google.golang.org/grpc"
 )
 
-func callTerraformer(config *pb.Config) *pb.Config {
+func callTerraformer(config *pb.Config) (*pb.Config, error) {
 	// Create connection to Terraformer
 	cc, err := grpc.Dial(urls.TerraformerURL, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("could not connect to Terraformer: %v", err)
+		return nil, fmt.Errorf("could not connect to Terraformer: %v", err)
 	}
 	defer cc.Close()
 	// Creating the client
 	c := pb.NewTerraformerServiceClient(cc)
 	res, err := terraformer.BuildInfrastructure(c, &pb.BuildInfrastructureRequest{Config: config})
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
-	return res.GetConfig()
+	return res.GetConfig(), nil
 }
 
-func callWireguardian(config *pb.Config) *pb.Config {
+func callWireguardian(config *pb.Config) (*pb.Config, error) {
 	cc, err := grpc.Dial(urls.WireguardianURL, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("could not connect to Wireguardian: %v", err)
+		return nil, fmt.Errorf("could not connect to Wireguardian: %v", err)
 	}
 	defer cc.Close()
 	// Creating the client
 	c := pb.NewWireguardianServiceClient(cc)
 	res, err := wireguardian.BuildVPN(c, &pb.BuildVPNRequest{Config: config})
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
-	return res.GetConfig()
+
+	return res.GetConfig(), nil
 }
 
-func callKubeEleven(config *pb.Config) *pb.Config {
+func callKubeEleven(config *pb.Config) (*pb.Config, error) {
 	cc, err := grpc.Dial(urls.KubeElevenURL, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("could not connect to KubeEleven: %v", err)
+		return nil, fmt.Errorf("could not connect to KubeEleven: %v", err)
 	}
 	defer cc.Close()
 	// Creating the client
 	c := pb.NewKubeElevenServiceClient(cc)
 	res, err := kubeEleven.BuildCluster(c, &pb.BuildClusterRequest{Config: config})
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
-	return res.GetConfig()
+
+	return res.GetConfig(), nil
 }
 
 // processConfig is function used to carry out task specific to Builder concurrently
-func processConfig(config *pb.Config, c pb.ContextBoxServiceClient) {
+func processConfig(config *pb.Config, c pb.ContextBoxServiceClient) (err error) {
 	log.Println("I got config: ", config.GetName())
-	config = callTerraformer(config)
-	config = callWireguardian(config)
-	config = callKubeEleven(config)
+
+	config, err = callTerraformer(config)
+	if err != nil {
+		return
+	}
+
+	config, err = callWireguardian(config)
+	if err != nil {
+		return
+	}
+
+	config, err = callKubeEleven(config)
+	if err != nil {
+		return
+	}
+
 	config.CurrentState = config.DesiredState // Update currentState
 
-	err := cbox.SaveConfigBuilder(c, &pb.SaveConfigRequest{Config: config})
+	err = cbox.SaveConfigBuilder(c, &pb.SaveConfigRequest{Config: config})
 	if err != nil {
-		log.Fatalln("Error while saving the config", err)
+		return fmt.Errorf("error while saving the config: %v", err)
 	}
+
+	return nil
 }
 
 // healthCheck function is function used for querring readiness of the pod running this microservice
@@ -121,13 +138,15 @@ func main() {
 		for {
 			res, err := cbox.GetConfigBuilder(c) // Get a new config
 			if err != nil {
-				log.Fatalln("Error while getting config from the Builder", err)
+				log.Println("Error while getting config from the Builder", err)
+				continue
 			}
 
 			config := res.GetConfig()
 			if config != nil {
 				go processConfig(config, c)
 			}
+
 			time.Sleep(5 * time.Second)
 		}
 	}()
@@ -136,5 +155,5 @@ func main() {
 	signal.Notify(ch, os.Interrupt)
 
 	<-ch
-	fmt.Println("Stopping Builder")
+	log.Println("Stopping Builder")
 }
