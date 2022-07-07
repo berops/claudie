faf4634514bbdc92fd54b7267f82370142f70aea
Miroslav Repka <repka438@gmail.com>
2022-07-07 13:18:06 +0200

    Add dynamically created credetials for Minio backend

--- services/terraformer/server/backend/backend.go
+++ services/terraformer/server/backend/backend.go
@@ -3,12 +3,14 @@ package backend
 import (
 	"fmt"
 
-	"github.com/Berops/platform/urls"
+	"github.com/Berops/platform/envs"
 	"github.com/Berops/platform/utils"
 )
 
 var (
-	minioURL = urls.MinioURL
+	minioURL  = envs.MinioURL
+	accessKey = envs.MinioAccessKey
+	secretKey = envs.MinioSecretKey
 )
 
 type Backend struct {
@@ -21,6 +23,8 @@ type templateData struct {
 	ProjectName string
 	ClusterName string
 	MinioURL    string
+	AccessKey   string
+	SecretKey   string
 }
 
 // function CreateFiles will create a backend.tf file from template
@@ -31,7 +35,7 @@ func (b Backend) CreateFiles() error {
 	if err != nil {
 		return fmt.Errorf("error while parsing template file backend.tpl: %v", err)
 	}
-	data := templateData{ProjectName: b.ProjectName, ClusterName: b.ClusterName, MinioURL: minioURL}
+	data := templateData{ProjectName: b.ProjectName, ClusterName: b.ClusterName, MinioURL: minioURL, AccessKey: accessKey, SecretKey: secretKey}
 	err = template.Generate(tpl, "backend.tf", data)
 	if err != nil {
 		return fmt.Errorf("error while creating backend files: %v", err)
