565b937054473a292b9351517c55625da6190525
Miroslav Repka <42786030+MiroslavRepka@users.noreply.github.com>
2022-08-18 16:15:52 +0200

    Feature/volatile secret data (#245)
    * Add kubectl apply for string variables
    
    * Add function which generates template to string
    
    * Change secret management to dynamic instead of file based
    
    * Remove unused const
    
    * Auto commit - add new kustomization.yaml
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: CI/CD pipeline <CI/CD-pipeline@users.noreply.github.com>

--- services/kuber/server/kubectl/kubectl_test.go
+++ services/kuber/server/kubectl/kubectl_test.go
@@ -8,10 +8,27 @@ import (
 
 //enter the test kubeconfig
 const config = ""
+const testStr = `apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: deny-from-other-namespaces
+  namespace: claudie
+spec:
+  ingress:
+  - from:
+    - podSelector: {}
+  podSelector:
+    matchLabels: null`
 
 func TestKubectlGet(t *testing.T) {
 	Kubectl := Kubectl{Kubeconfig: config}
 	_, err := Kubectl.KubectlGet("pods", "kube-system")
 	_, err1 := Kubectl.KubectlGet("pods", "")
 	require.NoError(t, err, err1)
+}
+
+func TestApplyString(t *testing.T) {
+	Kubectl := Kubectl{}
+	err := Kubectl.KubectlApplyString(testStr, "")
+	require.NoError(t, err)
 }
