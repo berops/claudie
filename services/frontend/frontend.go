928fa1fae4735a67c430c49a5c3a11d649eb9c85
Miroslav Repka <repka438@gmail.com>
2022-08-31 16:46:18 +0200

    Refactor Frontend

--- services/frontend/frontend.go
+++ services/frontend/frontend.go
@@ -12,100 +12,102 @@ import (
 
 	"github.com/rs/zerolog/log"
 	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
 	"gopkg.in/yaml.v3"
 
 	"github.com/Berops/platform/internal/envs"
 	"github.com/Berops/platform/internal/healthcheck"
 	"github.com/Berops/platform/internal/manifest"
 	"github.com/Berops/platform/internal/utils"
+	"github.com/Berops/platform/internal/worker"
 	"github.com/Berops/platform/proto/pb"
 	cbox "github.com/Berops/platform/services/context-box/client"
 )
 
 const (
 	defaultFrontendPort = 50058
-	sleepDuration       = 60 * 5 // 5 minutes
+	sleepDuration       = 5 // 5 minutes
 )
 
 var (
 	manifestDir = os.Getenv("MANIFEST_DIR")
 )
 
-func ClientConnection() pb.ContextBoxServiceClient {
+func ClientConnection() *grpc.ClientConn {
 	cc, err := utils.GrpcDialWithInsecure("context-box", envs.ContextBoxURL)
 	if err != nil {
 		log.Fatal().Err(err)
 	}
-	log.Info().Msg("Connected to cbox")
-
-	// Creating the client
-	c := pb.NewContextBoxServiceClient(cc)
-	return c
+	log.Info().Msg("Connected to context-box")
+	return cc
 }
 
-func SaveFiles(c pb.ContextBoxServiceClient) error {
-	if c == nil {
-		return fmt.Errorf("nil client received")
-	}
-
-	// loop through the directory and list files inside
-	files, err := ioutil.ReadDir(manifestDir)
-	if err != nil {
-		log.Fatal().Msgf("Error while trying to read test sets: %v", err)
-		return err
-	}
-
-	// get all saved configs
-	configsToDelete, err := cbox.GetAllConfigs(c)
-	if err != nil {
-		log.Fatal().Msgf("Failed to get all configs from the database")
-		return err
-	}
-
-	log.Info().Msgf("Found %d files in %v", len(files), manifestDir)
-	log.Info().Msgf("Found %d files in database", len(configsToDelete.Configs))
-
-	for _, file := range files {
-		// read file
-		var manifest manifest.Manifest
-		filePath := filepath.Join(manifestDir, file.Name())
-		strManifest, err := ioutil.ReadFile(filePath)
-		if err != nil {
-			log.Fatal().Err(err)
-			return err
+func saveFiles(c pb.ContextBoxServiceClient) func() error {
+	return func() error {
+		if c == nil {
+			return fmt.Errorf("nil client received")
 		}
-		// syntax check can be done here
-		err = yaml.Unmarshal([]byte(strManifest), &manifest)
-		if err != nil {
-			log.Fatal().Msgf("Failed to parse manifest file, Err:%v", err)
-			return err
-		}
+		go func() {
+			// loop through the directory and list files inside
+			files, err := ioutil.ReadDir(manifestDir)
+			if err != nil {
+				log.Error().Msgf("Error while trying to read test sets: %v", err)
+				return
+			}
 
-		// remove this config from configsToDelete
-		configsToDelete.Configs, err = removeConfig(configsToDelete.Configs, manifest.Name)
-		if err != nil {
-			log.Info().Msgf("No config saved in the database")
-		}
-		_, err = cbox.SaveConfigFrontEnd(c, &pb.SaveConfigRequest{
-			Config: &pb.Config{
-				Name:     manifest.Name,
-				Manifest: string(strManifest),
-			},
-		})
-		if err != nil {
-			log.Fatal().Msgf("Error while saving the config: %v err: %v", file.Name(), err)
-			return err
-		}
-		log.Info().Msgf("File %s has been saved to the database", filePath)
-	}
+			// get all saved configs
+			configsToDelete, err := cbox.GetAllConfigs(c)
+			if err != nil {
+				log.Error().Msgf("Failed to get all configs from the database")
+				return
+			}
 
-	for _, config := range configsToDelete.Configs {
-		if err := cbox.DeleteConfig(c, config.Id, pb.IdType_HASH); err != nil {
-			log.Error().Msgf("Failed to the delete %s with id %s : %v", config.Name, config.Id, err)
-		}
+			log.Info().Msgf("Found %d files in %v", len(files), manifestDir)
+			log.Info().Msgf("Found %d files in database", len(configsToDelete.Configs))
+
+			for _, file := range files {
+				// read file
+				var manifest manifest.Manifest
+				filePath := filepath.Join(manifestDir, file.Name())
+				strManifest, err := ioutil.ReadFile(filePath)
+				if err != nil {
+					log.Error().Err(err)
+					return
+				}
+				// syntax check can be done here
+				err = yaml.Unmarshal([]byte(strManifest), &manifest)
+				if err != nil {
+					log.Error().Msgf("Failed to parse manifest file, Err:%v", err)
+					return
+				}
+
+				// remove this config from configsToDelete
+				configsToDelete.Configs, err = removeConfig(configsToDelete.Configs, manifest.Name)
+				if err != nil {
+					log.Info().Msgf("No config saved in the database")
+				}
+				_, err = cbox.SaveConfigFrontEnd(c, &pb.SaveConfigRequest{
+					Config: &pb.Config{
+						Name:     manifest.Name,
+						Manifest: string(strManifest),
+					},
+				})
+				if err != nil {
+					log.Error().Msgf("Error while saving the config: %v err: %v", file.Name(), err)
+					return
+				}
+				log.Info().Msgf("File %s has been saved to the database", filePath)
+			}
+
+			for _, config := range configsToDelete.Configs {
+				if err := cbox.DeleteConfig(c, config.Id, pb.IdType_HASH); err != nil {
+					log.Error().Msgf("Failed to the delete %s with id %s : %v", config.Name, config.Id, err)
+				}
+			}
+			log.Info().Msg("Saved all files")
+		}()
+		return nil
 	}
-	log.Info().Msg("Saved all files")
-	return nil
 }
 
 func removeConfig(configs []*pb.Config, configName string) ([]*pb.Config, error) {
@@ -124,7 +126,7 @@ func removeConfig(configs []*pb.Config, configName string) ([]*pb.Config, error)
 }
 
 func healthCheck() error {
-	err := SaveFiles(nil)
+	err := saveFiles(nil)()
 	if err == nil {
 		return fmt.Errorf("health check function got unexpected result")
 	}
@@ -133,29 +135,27 @@ func healthCheck() error {
 
 func main() {
 	utils.InitLog("frontend")
-
-	client := ClientConnection()
-
+	cc := ClientConnection()
+	defer cc.Close()
+	// Creating the client
+	c := pb.NewContextBoxServiceClient(cc)
 	// Initialize health probes
 	healthChecker := healthcheck.NewClientHealthChecker(fmt.Sprint(defaultFrontendPort), healthCheck)
 	healthChecker.StartProbes()
 
-	g, _ := errgroup.WithContext(context.Background())
-
+	g, ctx := errgroup.WithContext(context.Background())
+	w := worker.NewWorker(ctx, sleepDuration*time.Second, saveFiles(c), worker.ErrorLogger)
+	// interrupt catching goroutine
 	g.Go(func() error {
 		ch := make(chan os.Signal, 1)
 		signal.Notify(ch, os.Interrupt)
 		defer signal.Stop(ch)
 		<-ch
-		return errors.New("scheduler interrupt signal")
+		return errors.New("builder interrupt signal")
 	})
-	for {
-		// list and upload manifest
-		err := SaveFiles(client)
-		if err != nil {
-			panic(err)
-		}
-		time.Sleep(time.Duration(sleepDuration * time.Second))
-	}
-
+	g.Go(func() error {
+		w.Run()
+		return nil
+	})
+	log.Info().Msgf("Stopping Frontend: %v", g.Wait())
 }
