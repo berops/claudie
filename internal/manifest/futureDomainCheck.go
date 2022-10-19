69f4682d9c653c533a7d52dc5f91bf242112e3cc
Miroslav Repka <repka438@gmail.com>
2022-10-19 19:23:47 +0100

    Implement new future domain check

--- internal/manifest/futureDomainCheck.go
+++ internal/manifest/futureDomainCheck.go
@@ -2,6 +2,7 @@ package manifest
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/Berops/claudie/internal/utils"
 )
@@ -11,23 +12,20 @@ const (
 	baseLength = 8 + 19 + 15
 )
 
-// CheckLengthOfFutureDomain will check if the possible domain name is too long
+// checkLengthOfFutureDomain will check if the possible domain name is too long
 // returns error if domain will be too long, nil if not
 // Described in https://github.com/Berops/claudie/issues/112#issuecomment-1015432224
 func checkLengthOfFutureDomain(m *Manifest) error {
 	// https://<public-ip>:6443/<api-path>/<node-name>
-	// <node-name> = clusterName + hash + nodeName + indexLength + separators
 	for _, cluster := range m.Kubernetes.Clusters {
 		for _, nodepoolName := range cluster.Pools.Control {
-			if total, err := m.NodePools.checkNodepoolDomain(nodepoolName, cluster.Name); err != nil {
-				return fmt.Errorf("cluster name %s or nodepool name %s is too long, consider shortening it to be bellow total node name bellow %d [total node name: %d, hash: %d]",
-					cluster.Name, nodepoolName, maxLength, total, utils.HashLength)
+			if err := m.NodePools.checkNodepoolDomain(nodepoolName, cluster.Name); err != nil {
+				return err
 			}
 		}
 		for _, nodepoolName := range cluster.Pools.Control {
-			if total, err := m.NodePools.checkNodepoolDomain(nodepoolName, cluster.Name); err != nil {
-				return fmt.Errorf("cluster name %s or nodepool name %s is too long, consider shortening it to be bellow total node name bellow %d [total node name: %d, hash: %d]",
-					cluster.Name, nodepoolName, maxLength, total, utils.HashLength)
+			if err := m.NodePools.checkNodepoolDomain(nodepoolName, cluster.Name); err != nil {
+				return err
 			}
 		}
 	}
@@ -35,6 +33,36 @@ func checkLengthOfFutureDomain(m *Manifest) error {
 	return nil
 }
 
-func (np *NodePool) checkNodepoolDomain(NodePoolName, clusterName string) (int, error) {
-	return 0, nil
+// checkNodepoolDomain check the future domain for the nodepool specified in nodepoolName parameter
+// returns nil if domain is not too long, error otherwise
+func (np *NodePool) checkNodepoolDomain(nodepoolName, clusterName string) error {
+	// <node-name> = clusterName + hash + nodeName + indexLength + separators
+	count := np.getCount(nodepoolName)
+	if count == -1 {
+		return fmt.Errorf("Nodepool with %s name was not found, cannot validate the future domain", nodepoolName)
+	}
+	total := 3 /*separator*/ + len(clusterName) + utils.HashLength + len(nodepoolName) + len(strconv.Itoa(count)) /*get length of the string*/
+	if total > maxLength {
+		return fmt.Errorf("cluster name %s or nodepool name %s is too long, consider shortening it to be bellow total node name bellow %d [total node name: %d, hash: %d]",
+			clusterName, nodepoolName, maxLength, total, utils.HashLength)
+	}
+	return nil
+}
+
+// getCount returns the count of the specified nodepool
+// for dynamic nodepools, returns the Count parameter
+// for static nodepools, returns length of the Nodes slice
+// if nodepool was not found, returns -1
+func (np *NodePool) getCount(nodepoolName string) int {
+	for _, n := range np.Dynamic {
+		if n.Name == nodepoolName {
+			return int(n.Count)
+		}
+	}
+	for _, n := range np.Static {
+		if n.Name == nodepoolName {
+			return len(n.Nodes)
+		}
+	}
+	return -1
 }
