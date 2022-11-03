f603a06241abe696decda139d62453338625131f
Miroslav Repka <repka438@gmail.com>
2022-11-03 15:05:26 +0000

    Add error check for package `copy`

--- internal/copy/copy_file.go
+++ internal/copy/copy_file.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+
+	"github.com/rs/zerolog/log"
 )
 
 // file copies a single file from src to dst
@@ -16,12 +18,20 @@ func file(src, dst string) error {
 	if srcfd, err = os.Open(src); err != nil {
 		return err
 	}
-	defer srcfd.Close()
+	defer func() {
+		if err := srcfd.Close(); err != nil {
+			log.Error().Msgf("Error while closing file: %v", err)
+		}
+	}()
 
 	if dstfd, err = os.Create(dst); err != nil {
 		return err
 	}
-	defer dstfd.Close()
+	defer func() {
+		if err := dstfd.Close(); err != nil {
+			log.Error().Msgf("Error while closing file: %v", err)
+		}
+	}()
 
 	if _, err = io.Copy(dstfd, srcfd); err != nil {
 		return err
@@ -41,7 +51,11 @@ func FileToDir(srcFile, dstDir string) error {
 	if srcfd, err = os.Open(srcFile); err != nil {
 		return err
 	}
-	defer srcfd.Close()
+	defer func() {
+		if err := srcfd.Close(); err != nil {
+			log.Error().Msgf("Error while closing file: %v", err)
+		}
+	}()
 
 	if info, err := os.Stat(dstDir); os.IsNotExist(err) {
 		if err = os.MkdirAll(dstDir, info.Mode()); err != nil {
@@ -52,7 +66,11 @@ func FileToDir(srcFile, dstDir string) error {
 	if dstfd, err = os.Create(filepath.Join(dstDir, srcFile)); err != nil {
 		return err
 	}
-	defer dstfd.Close()
+	defer func() {
+		if err := dstfd.Close(); err != nil {
+			log.Error().Msgf("Error while closing file: %v", err)
+		}
+	}()
 
 	if _, err = io.Copy(dstfd, srcfd); err != nil {
 		return err
