8260b703f04f47b9997b5f5df21e5294705bd8ef
Miroslav Repka <repka438@gmail.com>
2022-11-16 09:47:56 +0000

    Add missing imports

--- services/context-box/server/server.go
+++ services/context-box/server/server.go
@@ -20,7 +20,9 @@ import (
 	"github.com/Berops/claudie/internal/healthcheck"
 	"github.com/Berops/claudie/proto/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/status"
 )
 
 type server struct {
