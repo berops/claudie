1788008647a00b6e8dabf0b76a4b41ee32ead9be
Miroslav Repka <repka438@gmail.com>
2023-03-09 12:35:05 +0000

    Add test for autoscaler-adapter

---
+++ services/autoscaler-adapter/claudie_provider/claudie_provider_test.go
@@ -0,0 +1,32 @@
+package claudie_provider
+
+import (
+	"context"
+	"testing"
+
+	"github.com/berops/claudie/internal/utils"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/externalgrpc/protos"
+)
+
+func TestRefresh(t *testing.T) {
+	var cc *grpc.ClientConn
+	var err error
+	URL := "localhost:50000"
+
+	if cc, err = utils.GrpcDialWithInsecure("adapter", URL); err != nil {
+		t.Error(err)
+	}
+
+	defer func() {
+		if err := cc.Close(); err != nil {
+			log.Error().Msgf("Failed to close adapter connection %v", err)
+		}
+	}()
+
+	c := protos.NewCloudProviderClient(cc)
+	if _, err = c.Refresh(context.Background(), &protos.RefreshRequest{}); err != nil {
+		t.Error(err)
+	}
+}
