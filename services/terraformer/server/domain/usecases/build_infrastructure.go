580ccb3d07de47046dcc81a5eb5d4d8c007e3efc
Archisman-mridha <archismanmridha12345@gmail.com>
2023-05-11 17:18:04 +0530

    making build_infrastructure.go file more readable

--- services/terraformer/server/domain/usecases/build_infrastructure.go
+++ services/terraformer/server/domain/usecases/build_infrastructure.go
@@ -11,9 +11,12 @@ import (
 	"github.com/berops/claudie/services/terraformer/server/loadbalancer"
 )
 
-// BuildInfrastructure builds a single Kubernetes cluster
+// BuildInfrastructure builds the required infrastructure for a single Kubernetes cluster
 // and the Loadbalancer clusters related to it
 func (u *Usecases) BuildInfrastructure(request *pb.BuildInfrastructureRequest) (*pb.BuildInfrastructureResponse, error) {
+	// clusters slice contains those Kubernetes and Loadbalancer clusters
+	// for which infrastructure will be built.
+	// The "clusters" slice is initialized with the desired Kubernetes cluster we want to build.
 	clusters := []Cluster{
 		kubernetes.K8Scluster{
 			ProjectName: request.ProjectName,
@@ -25,23 +28,29 @@ func (u *Usecases) BuildInfrastructure(request *pb.BuildInfrastructureRequest) (
 		},
 	}
 
+	// LB clusters which appear in both request.CurrentLbs and request.DesiredLbs
+	// are appended to the "clusters" slice
 	for _, desiredLb := range request.DesiredLbs {
-		var curr *pb.LBcluster
-		for _, current := range request.CurrentLbs {
-			if desiredLb.ClusterInfo.Name == current.ClusterInfo.Name {
-				curr = current
+		var commonLb *pb.LBcluster
+
+		for _, currentLb := range request.CurrentLbs {
+			if desiredLb.ClusterInfo.Name == currentLb.ClusterInfo.Name {
+				commonLb = currentLb
 				break
 			}
 		}
-		clusters = append(clusters, loadbalancer.LBcluster{DesiredLB: desiredLb, CurrentLB: curr, ProjectName: request.ProjectName})
+
+		clusters = append(clusters, loadbalancer.LBcluster{DesiredLB: desiredLb, CurrentLB: commonLb, ProjectName: request.ProjectName})
 	}
 
+	// Concurrently build infrastructure for each cluster in the "clusters" slice
 	err := utils.ConcurrentExec(clusters, func(cluster Cluster) error {
 		log.Info().Msgf("Creating infrastructure for cluster %s project %s", cluster.Id(), request.ProjectName)
 
 		if err := cluster.Build(); err != nil {
 			return fmt.Errorf("error while building the cluster %v : %w", cluster.Id(), err)
 		}
+
 		log.Info().Msgf("Infrastructure was successfully created for cluster %s project %s", cluster.Id(), request.ProjectName)
 		return nil
 	})
