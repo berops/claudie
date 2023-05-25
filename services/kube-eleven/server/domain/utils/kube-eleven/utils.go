356ccb21f77995dac714a6048585c854b9fafdd3
__noob__coder__ <archismanmridha12345@gmail.com>
2023-05-25 16:07:30 +0530

    Refactoring kube-eleven microservice (#798)
    * save work
    
    * implementing clean architecture for the
    
    * Clean up
    
    * Auto commit - update kustomization.yaml
    
    * Fix template variable
    
    * Auto commit - update kustomization.yaml
    
    ---------
    
    Co-authored-by: Miroslav Repka <repka438@gmail.com>
    Co-authored-by: CI/CD pipeline <CI/CD-pipeline@users.noreply.github.com>

---
+++ services/kube-eleven/server/domain/utils/kube-eleven/utils.go
@@ -0,0 +1,28 @@
+package kube_eleven
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// readKubeconfigFromFile reads kubeconfig from a file and returns it as a string
+func readKubeconfigFromFile(path string) (string, error) {
+	kubeconfigAsByte, err := os.ReadFile(path)
+	if err != nil {
+		return "", fmt.Errorf("error while reading kubeconfig from file %s : %w", path, err)
+	}
+
+	return string(kubeconfigAsByte), nil
+}
+
+// sanitiseString replaces all white spaces and ":" in the string to "-".
+func sanitiseString(s string) string {
+	// convert to lower case
+	sanitised := strings.ToLower(s)
+	// replace all white space with "-"
+	sanitised = strings.ReplaceAll(sanitised, " ", "-")
+	// replace all ":" with "-"
+	sanitised = strings.ReplaceAll(sanitised, ":", "-")
+	return sanitised
+}
