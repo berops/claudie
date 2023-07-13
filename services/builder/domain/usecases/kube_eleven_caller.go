345ae7b83f906f8392a21ab8496c6344600f46d4
Miroslav Repka <repka438@gmail.com>
2023-07-13 19:20:03 +0200

    Create first callers

---
+++ services/builder/domain/usecases/kube_eleven_caller.go
@@ -0,0 +1,37 @@
+package usecases
+
+import (
+	"fmt"
+
+	cutils "github.com/berops/claudie/internal/utils"
+	"github.com/berops/claudie/proto/pb"
+	"github.com/berops/claudie/services/builder/domain/usecases/utils"
+)
+
+// callKubeEleven passes config to kubeEleven to bootstrap k8s cluster
+func (u *Usecases) buildK8sCluster(ctx *utils.BuilderContext) error {
+	logger := cutils.CreateLoggerWithProjectAndClusterName(ctx.ProjectName, ctx.GetClusterID())
+	cboxClient := u.ContextBox.GetClient()
+
+	description := ctx.Workflow.Description
+	ctx.Workflow.Stage = pb.Workflow_KUBE_ELEVEN
+	ctx.Workflow.Description = fmt.Sprintf("%s building kubernetes cluster", description)
+	if err := u.ContextBox.SaveWorkflowState(ctx.ProjectName, ctx.GetClusterName(), ctx.Workflow, cboxClient); err != nil {
+		return err
+	}
+
+	logger.Info().Msgf("Calling BuildCluster on Kube-eleven")
+	res, err := u.KubeEleven.BuildCluster(ctx, u.KubeEleven.GetClient())
+	if err != nil {
+		return fmt.Errorf("error while building kubernetes cluster %s project %s : %w", ctx.GetClusterID(), ctx.ProjectName, err)
+	}
+	logger.Info().Msgf("BuildCluster on Kube-eleven finished successfully")
+
+	ctx.DesiredCluster = res.Desired
+	ctx.DesiredLoadbalancers = res.DesiredLbs
+	ctx.Workflow.Description = description
+	if err := u.ContextBox.SaveWorkflowState(ctx.ProjectName, ctx.GetClusterName(), ctx.Workflow, cboxClient); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ services/builder/domain/usecases/terraformer_caller.go
@@ -0,0 +1,95 @@
+package usecases
+
+import (
+	"errors"
+	"fmt"
+
+	cutils "github.com/berops/claudie/internal/utils"
+	"github.com/berops/claudie/proto/pb"
+	"github.com/berops/claudie/services/builder/domain/usecases/utils"
+	terraformer "github.com/berops/claudie/services/terraformer/client"
+)
+
+var (
+	// ErrFailedToBuildInfrastructure is returned when the infra fails to build in terraformer
+	// including any partial failures.
+	ErrFailedToBuildInfrastructure = errors.New("failed to successfully build desired state")
+)
+
+func (u *Usecases) reconcileInfrastructure(ctx *utils.BuilderContext) error {
+	logger := cutils.CreateLoggerWithProjectAndClusterName(ctx.ProjectName, ctx.GetClusterID())
+	cboxClient := u.ContextBox.GetClient()
+
+	description := ctx.Workflow.Description
+	ctx.Workflow.Stage = pb.Workflow_TERRAFORMER
+	ctx.Workflow.Description = fmt.Sprintf("%s building infrastructure", description)
+	if err := u.ContextBox.SaveWorkflowState(ctx.ProjectName, ctx.GetClusterName(), ctx.Workflow, cboxClient); err != nil {
+		return err
+	}
+
+	logger.Info().Msgf("Calling BuildInfrastructure on Terraformer")
+
+	res, err := terraformer.BuildInfrastructure(u.Terraformer.GetClient(),
+		&pb.BuildInfrastructureRequest{
+			Current:     ctx.CurrentCluster,
+			Desired:     ctx.DesiredCluster,
+			CurrentLbs:  ctx.CurrentLoadbalancers,
+			DesiredLbs:  ctx.DesiredLoadbalancers,
+			ProjectName: ctx.ProjectName,
+		})
+	if err != nil {
+		return fmt.Errorf("error while reconciling infrastructure for cluster %s project %s : %w", ctx.GetClusterName(), ctx.ProjectName, err)
+	}
+
+	switch resp := res.GetResponse().(type) {
+	case *pb.BuildInfrastructureResponse_Fail:
+		logger.Error().Msgf("failed to build %s", resp.Fail.Failed)
+		ctx.CurrentCluster = resp.Fail.Current
+		ctx.DesiredCluster = resp.Fail.Desired
+		ctx.CurrentLoadbalancers = resp.Fail.CurrentLbs
+		ctx.DesiredLoadbalancers = resp.Fail.DesiredLbs
+
+		return ErrFailedToBuildInfrastructure
+	case *pb.BuildInfrastructureResponse_Ok:
+		ctx.CurrentCluster = resp.Ok.Current
+		ctx.DesiredCluster = resp.Ok.Desired
+		ctx.CurrentLoadbalancers = resp.Ok.CurrentLbs
+		ctx.DesiredLoadbalancers = resp.Ok.DesiredLbs
+	}
+
+	ctx.Workflow.Description = description
+	if err := u.ContextBox.SaveWorkflowState(ctx.ProjectName, ctx.GetClusterName(), ctx.Workflow, cboxClient); err != nil {
+		return err
+	}
+	logger.Info().Msgf("BuildInfrastructure on Terraformer finished successfully")
+	return nil
+}
+
+func (u *Usecases) destroyInfrastructure(ctx *utils.BuilderContext) error {
+	logger := cutils.CreateLoggerWithProjectAndClusterName(ctx.ProjectName, ctx.GetClusterID())
+	cboxClient := u.ContextBox.GetClient()
+
+	description := ctx.Workflow.Description
+	ctx.Workflow.Stage = pb.Workflow_DESTROY_TERRAFORMER
+	ctx.Workflow.Description = fmt.Sprintf("%s destroying infrastructure", description)
+	if err := u.ContextBox.SaveWorkflowState(ctx.ProjectName, ctx.GetClusterName(), ctx.Workflow, cboxClient); err != nil {
+		return err
+	}
+
+	logger.Info().Msg("Calling DestroyInfrastructure on Terraformer")
+
+	if _, err := terraformer.DestroyInfrastructure(u.Terraformer.GetClient(),
+		&pb.DestroyInfrastructureRequest{
+			ProjectName: ctx.ProjectName,
+			Current:     ctx.CurrentCluster,
+			CurrentLbs:  ctx.CurrentLoadbalancers,
+		}); err != nil {
+		return fmt.Errorf("error while destroying infrastructure for cluster %s project %s : %w", ctx.GetClusterName(), ctx.ProjectName, err)
+	}
+	ctx.Workflow.Description = description
+	if err := u.ContextBox.SaveWorkflowState(ctx.ProjectName, ctx.GetClusterName(), ctx.Workflow, cboxClient); err != nil {
+		return err
+	}
+	logger.Info().Msg("DestroyInfrastructure on Terraformer finished successfully")
+	return nil
+}
