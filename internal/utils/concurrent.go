704a20c03ca4ba49e72531af229dfaf2b6c59b9f
despire <matus.mrekaj@icloud.com>
2024-04-02 11:06:45 +0200

    update deps

--- internal/utils/concurrent.go
+++ internal/utils/concurrent.go
@@ -8,11 +8,9 @@ func ConcurrentExec[K any](items []K, f func(index int, item K) error) error {
 	group := errgroup.Group{}
 
 	for i, item := range items {
-		func(index int, item K) {
-			group.Go(func() error {
-				return f(index, item)
-			})
-		}(i, item)
+		group.Go(func() error {
+			return f(i, item)
+		})
 	}
 
 	return group.Wait()
