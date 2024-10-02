0b368826e38faa276cc4635b57a7baa149401f46
despire <matus.mrekaj@icloud.com>
2024-10-02 21:22:34 +0200

    update testing framework

--- services/testing-framework/utils.go
+++ services/testing-framework/utils.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	maxTimeout     = 8000    // max allowed time for one manifest to finish in [seconds]
+	maxTimeout     = 21_000  // max allowed time for one manifest to finish in [seconds]
 	sleepSec       = 30      // seconds for one cycle of config check
 	maxTimeoutSave = 60 * 12 // max allowed time for config to be found in the database
 )
