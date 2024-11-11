792762e023a006dabcec58bbb3ceb6a8b6c628c8
despire <matus.mrekaj@icloud.com>
2024-11-11 14:48:04 +0100

    revert changes to testing-framework

--- services/testing-framework/claudie_test.go
+++ services/testing-framework/claudie_test.go
@@ -249,15 +249,12 @@ func processTestSet(
 		if errCleanUp != nil {
 			if errors.Is(errCleanUp, errInterrupt) {
 				log.Warn().Msgf("Testing-framework received interrupt signal, aborting test checking")
-				// On interrupt remove infra
+				// Do not return error, since it was an interrupt
 				return nil
 			}
 			if i != len(configs)-1 && continueOnBuildError {
 				continue
 			}
-			// do not delete infra so that we can debug what went wrong, the deletion is then needed to be done
-			// manually.
-			errCleanUp = nil
 			return fmt.Errorf("error while monitoring manifest %s from test set %s : %w", entry.Name(), setName, errCleanUp)
 		}
 
