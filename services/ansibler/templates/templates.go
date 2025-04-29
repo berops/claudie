a53ebd18dd6693ec3843050eeecef897dc5d2911
despire <matus.mrekaj@icloud.com>
2025-04-29 17:02:04 +0200

    remove nginx templates

--- services/ansibler/templates/templates.go
+++ services/ansibler/templates/templates.go
@@ -6,9 +6,6 @@ var (
 	//go:embed all-node-inventory.goini
 	AllNodesInventoryTemplate string
 
-	//go:embed conf.gotpl
-	NginxConfigTemplate string
-
 	//go:embed deploy-envoy.goyml
 	EnvoyTemplate string
 
@@ -26,9 +23,6 @@ var (
 
 	//go:embed lb-inventory.goini
 	LoadbalancerInventoryTemplate string
-
-	//go:embed nginx.goyml
-	NginxPlaybookTemplate string
 
 	//go:embed node-exporter.goyml
 	NodeExporterPlaybookTemplate string
