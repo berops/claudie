4606ee898b9a943174838118d6640a478a38387d
despire <matus.mrekaj@icloud.com>
2025-07-22 08:16:04 +0200

    update address

--- services/autoscaler-adapter/main.go
+++ services/autoscaler-adapter/main.go
@@ -49,7 +49,7 @@ func main() {
 		pprofMux.Handle("/debug/pprof/allocs", pprof.Handler("allocs")).Methods(http.MethodGet)
 		pprofMux.Handle("/debug/pprof/mutex", pprof.Handler("mutex")).Methods(http.MethodGet)
 
-		http.ListenAndServe("127.0.0.1:"+"18000", pprofMux)
+		http.ListenAndServe("0.0.0.0:"+"18000", pprofMux)
 	}()
 
 	// Listen
